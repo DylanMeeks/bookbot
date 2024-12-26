70743875a20a9fdced9dfb8fb4d7478c9a59267b
Dylan Meeks <dylan.meeks@aol.com>
2024-12-25 18:12:12 -0600

    number of character appearences

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     print(file_contents)
-    print(num_words(file_contents))
+    print(f"# of words: {num_words(file_contents)}")
+
+    char_appear = num_char_appear(file_contents)
+    print(char_appear)
 
 def num_words(text):
     words = text.split()
     return len(words)
+
+def num_char_appear(text):
+    appearences = {}
+    for char in text.lower():
+        appearences[char] += 1
+
+    return appearences
 
 main()
